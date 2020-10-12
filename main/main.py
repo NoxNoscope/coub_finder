1120135cf8ef7cbdfcf9f37f188f0f0fafb3a588
NoxNoscope <Lennart.Diego.kahn@gmail.com>
2020-10-12 23:59:52 +0200

    made the folders look nicer

--- main/main.py
+++ main/main.py
@@ -1,8 +1,8 @@
 from PyQt5 import QtWidgets, uic
 import sys
 from PyQt5.QtGui import *
-from recurces.lib_thingy.coub_dl import Coub
-from recurces.theme import Theme
+from main.recurces.lib_thingy.coub_dl import Coub
+from main.recurces.theme import Theme
 
 
 
