28af094f516de3797b5b5bb9e6190cc6d43469ad
NoxNoscope <Lennart.Diego.kahn@gmail.com>
2020-10-15 11:12:19 +0200

    added checkbox functionality

--- main.py
+++ main.py
@@ -9,49 +9,51 @@ from recurces.lib_thingy.coub_dl import Coub
 from recurces.theme import Theme
 
 
-
 class Ui(QtWidgets.QMainWindow):
-    def __init__(self):
-        super(Ui, self).__init__() # Call the inherited classes __init__ method
-        uic.loadUi('main_window.ui', self) # Load the .ui file
+	def __init__(self):
+		super(Ui, self).__init__()  # Call the inherited classes __init__ method
+		uic.loadUi('main_window.ui', self)  # Load the .ui file
+		self.downloadbtn.setIcon(QIcon('recurces/quicDownloadBtn.png'))
+		
+		self.mediaPlayer = QtMultimedia.QMediaPlayer(self)
+		self.mediaPlayer.setVideoOutput(self.videowidget)
+		
+		self.audioCheckBox.setChecked(True)
+		
+		self.show()  # Show the GUI
+		
+		self.downloadbtn.clicked.connect(self.download)
+		self.restartButton.clicked.connect(self.restart_program)
+	
+	def download(self):
+		url = self.lineEdit.text()
+		
+		coub = Coub()
+		if self.audioCheckBox.isChecked():
+			coub.audio(url, addlink=True)
+		
+		if self.videoCheckBox.isChecked():
+			coub.video(url, addlink=True)
+		
+		coub.lowvideo(url, addlink=False)
+		
+		self.lineEdit.clear()
+		
+		self.mediaPlayer.setMedia(QMediaContent(QUrl.fromLocalFile(os.getcwd() + "/tmp/tmp.mp4")))
+		self.mediaPlayer.play()
+	
+	def restart_program(self):
+		"""Restarts the current program, with file objects and descriptors
+		   cleanup
+		"""
+		os.execl(sys.executable, *([sys.executable] + sys.argv))
 
-        self.show() # Show the GUI
-        self.downloadbtn.setIcon(QIcon('recurces/quicDownloadBtn.png'))
 
-        self.mediaPlayer = QtMultimedia.QMediaPlayer(self)
-        self.mediaPlayer.setVideoOutput(self.videowidget)
-        
-        
-        self.downloadbtn.clicked.connect(self.download)
-        self.restartButton.clicked.connect(self.restart_program)
+app = QtWidgets.QApplication(sys.argv)  # Create an instance of QtWidgets.QApplication
 
-
-    def download(self):
-        url = self.lineEdit.text()
-
-        coub = Coub()
-        coub.audio(url, addlink=True)
-        #coub.video(url, addlink=True)
-        
-        
-        coub.lowvideo(url, addlink=False)
-        
-        self.lineEdit.clear()
-        
-        self.mediaPlayer.setMedia(QMediaContent(QUrl.fromLocalFile(os.getcwd() + "/tmp/tmp.mp4")))
-        self.mediaPlayer.play()
-        
-    def restart_program(self):
-        """Restarts the current program, with file objects and descriptors
-           cleanup
-        """
-        os.execl(sys.executable, *([sys.executable] + sys.argv))
-
-app = QtWidgets.QApplication(sys.argv) # Create an instance of QtWidgets.QApplication
-
-app.setStyle("Fusion") # adding some style that makes more things dark
+app.setStyle("Fusion")  # adding some style that makes more things dark
 them = Theme()
-app.setPalette(them.dark()) # adding the dark theme thats currently in that file, it can be modified to anything of anyones liking
+app.setPalette(them.dark())  # adding the dark theme thats currently in that file, it can be modified to anything
 
-window = Ui() # Create an instance of our class
-app.exec_() # Start the application
+window = Ui()  # Create an instance of our class
+app.exec_()  # Start the application
