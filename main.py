dd6fbf674247c74a5db3f0da5637f537e372ebdc
NoxNoscope <Lennart.Diego.kahn@gmail.com>
2020-10-15 13:00:23 +0200

    made checkboxes work with yaml and made then work with the app itself

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import os
 from PyQt5.QtCore import QUrl
 from PyQt5.QtGui import *
 from PyQt5.QtMultimedia import QMediaContent
+import yaml
 
 from recurces.lib_thingy.coub_dl import Coub
 from recurces.theme import Theme
@@ -11,6 +12,8 @@ from recurces.theme import Theme
 
 class Ui(QtWidgets.QMainWindow):
 	def __init__(self):
+		self.setting = self.yaml_load()
+		
 		super(Ui, self).__init__()  # Call the inherited classes __init__ method
 		uic.loadUi('main_window.ui', self)  # Load the .ui file
 		self.downloadbtn.setIcon(QIcon('recurces/quicDownloadBtn.png'))
@@ -18,14 +21,20 @@ class Ui(QtWidgets.QMainWindow):
 		self.mediaPlayer = QtMultimedia.QMediaPlayer(self)
 		self.mediaPlayer.setVideoOutput(self.videowidget)
 		
-		self.audioCheckBox.setChecked(True)
+		self.audioCheckBox.setChecked(self.setting["audioCheckBox"])
+		self.videoCheckBox.setChecked(self.setting["videoCheckBox"])
+		self.previewCheckBox.setChecked(self.setting["previewCheckBox"])
 		
 		self.show()  # Show the GUI
 		
 		self.downloadbtn.clicked.connect(self.download)
 		self.restartButton.clicked.connect(self.restart_program)
+		self.audioCheckBox.stateChanged.connect(self.audioCheckBox_toggle)
+		self.videoCheckBox.stateChanged.connect(self.videoCheckBox_toggle)
+		self.previewCheckBox.stateChanged.connect(self.previewCheckBox_toggle)
 	
 	def download(self):
+		
 		url = self.lineEdit.text()
 		
 		coub = Coub()
@@ -35,18 +44,59 @@ class Ui(QtWidgets.QMainWindow):
 		if self.videoCheckBox.isChecked():
 			coub.video(url, addlink=True)
 		
-		coub.lowvideo(url, addlink=False)
+		if self.previewCheckBox.isChecked():
+			coub.lowvideo(url, addlink=False)
+			self.mediaPlayer.setMedia(QMediaContent(QUrl.fromLocalFile(os.getcwd() + "/tmp/tmp.mp4")))
+			self.mediaPlayer.play()
 		
 		self.lineEdit.clear()
-		
-		self.mediaPlayer.setMedia(QMediaContent(QUrl.fromLocalFile(os.getcwd() + "/tmp/tmp.mp4")))
-		self.mediaPlayer.play()
+	
+	def save_checkbox_settings(self):
+		# if self.audioCheckBox.isChecked():
+		pass
 	
 	def restart_program(self):
 		"""Restarts the current program, with file objects and descriptors
 		   cleanup
 		"""
 		os.execl(sys.executable, *([sys.executable] + sys.argv))
+	
+	def audioCheckBox_toggle(self):
+		if self.setting["audioCheckBox"] == False:
+			self.setting["audioCheckBox"] = True
+		
+		elif self.setting["audioCheckBox"] == True:
+			self.setting["audioCheckBox"] = False
+		
+		self.yaml_dump(self.setting)
+	
+	def videoCheckBox_toggle(self):
+		if self.setting["videoCheckBox"] == False:
+			self.setting["videoCheckBox"] = True
+		
+		elif self.setting["videoCheckBox"] == True:
+			self.setting["videoCheckBox"] = False
+		
+		self.yaml_dump(self.setting)
+	
+	def previewCheckBox_toggle(self):
+		if self.setting["previewCheckBox"] == False:
+			self.setting["previewCheckBox"] = True
+		
+		elif self.setting["previewCheckBox"] == True:
+			self.setting["previewCheckBox"] = False
+		
+		self.yaml_dump(self.setting)
+	
+	def yaml_load(self):
+		with open('recurces/setting.yaml', "r") as stream:
+			setting = yaml.full_load(stream)
+		return setting
+	
+	def yaml_dump(self, data):
+		print(self.setting)
+		with open('recurces/setting.yaml', 'w') as stream:
+			yaml.dump(data, stream)
 
 
 app = QtWidgets.QApplication(sys.argv)  # Create an instance of QtWidgets.QApplication
